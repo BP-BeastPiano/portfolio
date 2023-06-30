a82dea5130670220ddaed591736c7393e789e7c9
Harshilport <103949058+Harshilport@users.noreply.github.com>
2023-06-30 20:03:00 +0530

    s

--- models/Team.js
+++ models/Team.js
@@ -6,7 +6,8 @@ const TeamSchema = new mongoose.Schema(
     img: { type: String, required: true },
     path: { type: String, required: true },
     link: { type: String, required: true },
-    role: { type: String, required: true }
+    role: { type: String, required: true },
+    qualifications: { type: Array }
   },
   { timestamps: true }
 );
